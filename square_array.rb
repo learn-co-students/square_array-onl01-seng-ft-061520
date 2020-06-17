8078f17d81e89d03cfbed5a55a03864613917c79
TaeVon Lewis <lewistpro@gmail.com>
2020-06-17 22:45:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
-  Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }
-  end
-  .take(array.length)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
-
-my_arr = [1, 2, 3, 4]
-p square_array(my_arr) #=> [1, 4, 9, 16]
