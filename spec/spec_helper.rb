fdc9c01c80fdfa2b20bbd161d5b0d15f15bb53c1
Benjamin Anderson <benjaminghray@gmail.com>
2020-06-20 04:13:02 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,13 @@ RSpec.configure do |config|
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
-  #     --seed 1234
+  #     --seed 1234return gem_original_require(path)
   # config.order = 'random'def using_until
 	     levitation_force = 6
 	     until levitation_force == 10
 	        puts "Wingardium Leviosa"
 	        levitation_force += 1
-	        return gem_original_require(path)
+	        
 	        
 	    end
 end
