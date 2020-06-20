1be9ee1d9a60d0170bb004e74d3f4a9820abe829
Benjamin Anderson <benjaminghray@gmail.com>
2020-06-20 05:15:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,3 @@
 def square_array
-  return end
-	    
-gh pr [status, list, view, checkout, create]
-gh issue [status, list, view, create]
-gh repo [view, create, clone, fork]
-gh config [get, set]
-gh help
+  
+  
