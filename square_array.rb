f95dcbac12219f31c8a426c12359112a93103ea0
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-27 16:02:48 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,4 @@
 # def square_array(array) # your code here
-# square_array = []
-#    array.each do |num|
-#       square_array  << num * num
-#
-# end
-# return square_array
-# end
 
 def square_array(array_of_numbers)
   square_array = []
